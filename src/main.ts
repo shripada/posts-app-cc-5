6739b40369d411b23ef5174d1da67a8705d932a5
shripada <shripada.hebbar@codecraft.co.in>
2024-06-24 17:02:50 +0530

    initial checkin

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './style.css';
+import './posts.css';
+import typescriptLogo from './typescript.svg';
+import viteLogo from '/vite.svg';
+import { setupCounter } from './counter.ts';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="container">
+  <section>
+  <nav>
+   <button>< previous</button>
+   <h2>Post title</h2>
+   <button>next ></button>
+  </nav>
+  <p class="post-desc"> Post description</p>
+    </section>
+      <section>
+      <button> View Comments </button>
+      <p class="comments">Comments of current post go here</p>
+  </section>
+  </div>
+`;
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
